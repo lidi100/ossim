402ef494641185ff0eab67b6f23c826fec4f8a33
Mitul Patel <mitul.patel@digitalglobe.com>
2018-02-21 15:07:26 -0500

    make VERBOSE=1

--- scripts/build.sh
+++ scripts/build.sh
@@ -76,7 +76,7 @@ fi
 # CMake successful, now run make in the build directory (OSSIM_BUILD_DIR 
 # exported by cmake config script):
 pushd $OSSIM_BUILD_DIR >/dev/null
-make $MAKE_VERBOSE
+make VERBOSE=1
 if [ $? -ne 0 ]; then
   echo; echo "Error encountered during make. Check the console log and correct."
   popd>/dev/null
